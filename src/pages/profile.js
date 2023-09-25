0630a68f89c16d8e050d2361db73008dcb820e6f
Tanishq Pandey <tanishqvatsa26@gmail.com>
2023-09-25 16:44:43 +0530

    post search box added

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,53 +1,52 @@
-import { Link } from "react-router-dom";
-
-import { useNavigate } from "react-router-dom";
-
-import {useAuthState} from "react-firebase-hooks/auth";
-
+import React, { useEffect, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { signOut } from "firebase/auth";
-
-import {IoCreateOutline,IoLogOutOutline} from "react-icons/io5"
-
-import {BiTrash} from "react-icons/bi";
-
-import React , {useEffect, useState} from "react";
+import { collection, getDocs, deleteDoc, doc } from "firebase/firestore";
+import { IoCreateOutline, IoLogOutOutline } from "react-icons/io5";
+import { BiTrash } from "react-icons/bi";
 
 import Main from "./main";
-
 import Loginpage from "./loginpage";
-
 import CreatePost from "./createpost";
-
 import About from "./about";
-
-import { collection, getDocs ,deleteDoc,doc, addDoc } from "firebase/firestore";
-
-import { auth,db } from "../config/firebase";
-
-import {signUserOut} from "../App"
+import { auth, db } from "../config/firebase";
 
 
-function Profile(isAuth){
+function Profile(isAuth) {
+    const [postLists, setPostList] = useState([]);
+    const [searchQuery, setSearchQuery] = useState(""); // Added state for search
 
-        const [postLists, setPostList]=useState([]);
-       
-        const postsCollectionRef = collection(db, "posts");
+    const postsCollectionRef = collection(db, "posts");
 
-        useEffect(() => {
-                const getPosts = async () => {
-                  const data = await getDocs(postsCollectionRef);
-                //  console.log(data);
-                  setPostList(data.docs.map((doc)=> ({...doc.data(), id:doc.id  })));
-                };
+    const deletePost = async (id) => {
+        const postDoc = doc(db, "posts", id);
+        await deleteDoc(postDoc);
+        window.location.reload();
+        alert("Your Post was deleted!");
+    };
 
-                getPosts();
-        },[]);
+    useEffect(() => {
+        const getPosts = async () => {
+            const data = await getDocs(postsCollectionRef);
+            setPostList(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+        };
 
+        getPosts();
+    }, []);
 
-    const navigate= useNavigate();
-
+    const navigate = useNavigate();
     const [user] = useAuthState(auth);
- 
+
+    const filteredPosts = postLists.filter(post => {
+        return (
+            post.institute.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            post.coursename.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            post.postText.toLowerCase().includes(searchQuery.toLowerCase()) ||
+            post.author.name.toLowerCase().includes(searchQuery.toLowerCase())
+        );
+    });
+
     const signUserOut=async()=>{
       signOut(auth).then(() => {
         localStorage.clear();
@@ -58,102 +57,77 @@ function Profile(isAuth){
     }
   
 
-
-    const deletePost=async(id)=>{
-        const postDoc= doc(db, "posts", id);
-        await deleteDoc(postDoc);
-        
-    }
-
-    
-
     return (
-        <div >  
+        <div>
+            <header className="header">
+                <a href="#" className="logo">
+                    <h2>CotLog</h2>
+                </a>
+                <nav className="navbar-login">
+                    {user && (
+                        <>
+                            <img src={auth.currentUser?.photoURL || ""} width="25" height="25" />
+                            <p>{auth.currentUser?.displayName}</p>
+                            <Link to="/createpost" className="loginpage-link1">
+                                <IoCreateOutline />
+                            </Link>
+                            <Link to="/" className="loginpage-link2" onClick={signUserOut}>
+                                <IoLogOutOutline />
+                            </Link>
+                        </>
+                    )}
+                </nav>
+            </header>
 
-
-
-
-<div>
-
-<header className="header">
-        <a href="#" className="logo"><h2>CotLog</h2></a>
-
-<nav className="navbar-login">
-  
-  {user && (
-        <>
-        <img src={auth.currentUser?.photoURL || ""} width="25"  height="25" />
-          <p>{auth.currentUser?.displayName}</p>
-        <Link to="/createpost" className="loginpage-link1"><IoCreateOutline/></Link>
-        <Link  to="/" className="loginpage-link2"    onClick={signUserOut}><IoLogOutOutline/></Link>
-        </>
-  )}
-        
-        </nav>
-        </header>
-        
-</div>   
-
-
-
-                {console.log(auth.currentUser.uid)}
-
-<div className="loginPage">
-        {postLists.map((post)=>{
-                 return (
-
-                        <div className="postlp">
-            <div className="postHeader">
-              <div className="title">
-                <h1> {post.institute}</h1>
-              </div>
-
-              <div className="deletePost">
-                {isAuth && post.author.id === auth.currentUser.uid && (
-                  <button
-                    onClick={() => {
-                      deletePost(post.id);
-                    }}
-                  >
-                    {" "}
-                    <BiTrash/>
-                  </button>
-                )}
-              </div> 
-
-              <br></br>
-
-              <div className="title"><h2>{post.coursename}</h2></div>
-              <br></br>
-              <div className="postTextContainer"> {post.postText}
-               </div>
-              <br></br>
-
-            <h3>@{post.author.name}</h3>
-
-
-               
+            {/* Search box */}
+            <div className="search-bar-container">
+                <input
+                    type="text"
+                    placeholder="Search by College, Course, Description, Author..."
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                />
             </div>
-           
-            
-          </div>
 
+            <div className="profilePage">
+                {filteredPosts.map((post) => {
+                    return (
+                        <div className="postlp">
+                            <div className="postHeader">
+                                <div className="title">
+                                    <h1> {post.institute}</h1>
+                                </div>
 
-        );
-        })}
-</div>
+                                <div className="deletePost">
+                                    {isAuth && post.author.id === auth.currentUser.uid && (
+                                        <button
+                                            onClick={() => {
+                                                deletePost(post.id);
+                                            }}
+                                        >
+                                            {" "}
+                                            <BiTrash />
+                                        </button>
+                                    )}
+                                </div>
 
+                                <br></br>
 
+                                <div className="title">
+                                    <h2>{post.coursename}</h2>
+                                </div>
+                                <br></br>
+                                <div className="postTextContainer"> {post.postText}</div>
+                                <br></br>
 
-
-
-
-
-
-
+                                <h3>@{post.author.name}</h3>
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
         </div>
     );
-   
 }
 
 export default Profile;
